24ecf9cf184e6c0619b475178bb3b53cee8cbeb3
rohankanaujia90 <rohankanaujia90@gmail.com>
2025-11-27 15:29:02 +0530

    fibonacci

--- functions.cpp
+++ functions.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printNum(int n){
-    cout << n << endl;
+// void printNum(int n){
+//     cout << n << endl;
+// }
+// int main(){
+//     printNum(5);
+//     return 0;
+// }
+void fibonacci(int n){
+    int a=0,b=1,c;
+    for(int i=0;i<n;i++){
+        cout << a << " ";
+        c=a+b;
+        a=b;
+        b=c;
+    }
 }
 int main(){
-    printNum(5);
+    int n;
+    cin >> n;
+    fibonacci(n);
     return 0;
 }
