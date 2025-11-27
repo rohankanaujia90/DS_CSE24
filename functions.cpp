b433550aca19deb8157f1e9346a2c1d0befbae8e
rohankanaujia90 <rohankanaujia90@gmail.com>
2025-11-27 15:56:08 +0530

    palindrome

--- functions.cpp
+++ functions.cpp
@@ -8,21 +8,21 @@ using namespace std;
 //     printNum(5);
 //     return 0;
 // }
-void fibonacci(int n){
-    int a=0,b=1,c;
-    for(int i=0;i<n;i++){
-        cout << a << " ";
-        c=a+b;
-        a=b;
-        b=c;
-    }
-}
-int main(){
-    int n;
-    cin >> n;
-    fibonacci(n);
-    return 0;
-}
+// void fibonacci(int n){
+//     int a=0,b=1,c;
+//     for(int i=0;i<n;i++){
+//         cout << a << " ";
+//         c=a+b;
+//         a=b;
+//         b=c;
+//     }
+// }
+// int main(){
+//     int n;
+//     cin >> n;
+//     fibonacci(n);
+//     return 0;
+// }
 
 // int factorial(int n){
 //     if(n==0 || n==1){
@@ -38,3 +38,20 @@ int main(){
 //     cout << factorial(n) << endl;
 //     return 0;
 // }
+
+void palindrome(string str){
+    string rev = str;
+    reverse(rev.begin(), rev.end());
+    if(str == rev){
+        cout << "Palindrome" << endl;
+    } else {
+        cout << "Not a Palindrome" << endl;
+    }
+}
+int main(){
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+    palindrome(str);
+    return 0;
+}
