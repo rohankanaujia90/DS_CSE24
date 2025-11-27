d7edba153f67190e74e61cf8d9565118bc5b3232
rohankanaujia90 <rohankanaujia90@gmail.com>
2025-11-27 15:35:56 +0530

    function2

--- functions.cpp
+++ functions.cpp
@@ -23,3 +23,18 @@ int main(){
     fibonacci(n);
     return 0;
 }
+
+// int factorial(int n){
+//     if(n==0 || n==1){
+//         return 1;
+//     } 
+//     else {
+//         return n*factorial(n-1);
+//     } 
+// }
+// int main(){
+//     int n;
+//     cin >> n;
+//     cout << factorial(n) << endl;
+//     return 0;
+// }
