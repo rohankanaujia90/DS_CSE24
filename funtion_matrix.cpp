f1ba4bf98f2865051ca77117cdf81b6c2cbb3aab
rohankanaujia90 <rohankanaujia90@gmail.com>
2025-11-28 15:35:32 +0530

    matrix_multi

--- funtion_matrix.cpp
+++ funtion_matrix.cpp
@@ -1,23 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void matrix_sum(){
-    int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int b [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int c [3][3] ;
-    for(int  i= 0 ; i <3 ; i++ ){
-        for(int j=0 ; j < 3 ;  j++){
+
+
+// void matrix_sum(){
+//     int a [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//     int b [3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//     int c [3][3] ;
+//     for(int  i= 0 ; i <3 ; i++ ){
+//         for(int j=0 ; j < 3 ;  j++){
             
-            c[i][j]=a[i][j]+b[i][j];
+//             c[i][j]=a[i][j]+b[i][j];
             
-            cout<<c[i][j]<<" ";
-        }
+//             cout<<c[i][j]<<" ";
+//         }
         
-        cout<< endl;
+//         cout<< endl;
+//     }
+// }
+
+// int main(){
+//     matrix_sum();
+//     return 0 ;
+// }
+
+void matrix_multi(int A[][3], int B[][3] ){
+    int result [3][3];
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            result[i][j] = 0;
+            for(int k = 0; k < 3; k++) {
+                result[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    cout << "Matrix Multiplication Result:\n";
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
-int main(){
-    matrix_sum();
-    return 0 ;
+int main() {
+    int A[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    int B[3][3] = { {9, 8, 7}, {6, 5, 4}, {3, 2, 1} };
+    matrix_multi(A, B);
+    return 0;
 }
