5c59a4a2318ac936b1b5ed2708b10a4f290de9bf
rohankanaujia90 <rohankanaujia90@gmail.com>
2025-11-29 10:05:04 +0530

    call by reference

--- swap_call_by.cpp
+++ swap_call_by.cpp
@@ -1,15 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-void swap(int x,int y){
-    int t = x;
-    x=y;
-    y=t;
-    cout<<"after swap function x: "<< x << "\n y: " << y << endl;
-}
-int main(){
-    int x=5, y=10;
-    cout<<"before swap function x: "<< x << "\n y: " << y << endl;
-    swap(x,y);
-    cout<<"after swap function in main x: "<< x << "\n y: " << y << endl;
-    return 0;  
-}
+//call by value
+// void swap(int x,int y){
+//     int t = x;
+//     x=y;
+//     y=t;
+//     cout<<"after swap function x: "<< x << "\n y: " << y << endl;
+// }
+// int main(){
+//     int x=5, y=10;
+//     cout<<"before swap function x: "<< x << "\n y: " << y << endl;
+//     swap(x,y);
+//     cout<<"after swap function in main x: "<< x << "\n y: " << y << endl;
+//     return 0;  
+// }
+
+
+ //call by reference
+ void func(int &x){
+    x--;
+ }
+ int main(){
+    int a = 5;
+    cout << a << endl;
+    func(a);
+    cout << a << endl;
+ }
